b7eb0a4af87bd2f320d2b01b43d9ca8c45b1852e
ManiyaTariq <162306425+ManiyaTariq@users.noreply.github.com>
2024-05-15 22:20:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// import inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+// display a colorfull wellcome message
+console.log(chalk.bold.cyanBright("\n \t\t Welcome to my Word - Counter"));
+console.log("=".repeat(60));
+//prompt the user to enter the sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+// trimming the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+// analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
